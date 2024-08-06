71772c044951130fa9591d99f6cc657993c5d87f
Juan Pedro <jppontverges@gmail.com>
2024-08-05 23:54:29 -0300

    a

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,6 +7,24 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata: Metadata = {
   title: "Mushi - Juanshi Tracker 🍄‍🟫🐨",
   description: "Everyday that goes by, Juanshi gets closer to Mushi hunter",
+  openGraph: {
+    title: "Mushi - Juanshi Tracker 🍄‍🟫🐨",
+    description: "Everyday that goes by, Juanshi gets closer to Mushi hunter",
+    siteName: "Mushi - Juanshi Tracker 🍄‍🟫🐨",
+    images: [
+      {
+        url: '/opengraph-image.jpeg',
+        width: 800,
+        height: 600,
+      },
+      {
+        url: '/opengraph-image.jpeg',
+        width: 1800,
+        height: 1600,
+      },
+    ],
+    type: 'website',
+  },
 };
 
 export default function RootLayout({
